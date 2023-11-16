4de66c9706f07e5f8cfa8f37c30713534426bda8
cheikh778 <locheikhndao778@gmail.com>
2023-11-16 19:04:27 +0000

    commit pour le nouveau dashboard
    Signed-off-by: cheikh778 <locheikhndao778@gmail.com>

--- src/app/Paysan/ajout-culture/ajout-culture.component.ts
+++ src/app/Paysan/ajout-culture/ajout-culture.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AjoutCultureComponent {
 
+  status = false;
+  showNotificationDropdown = false;
+  showProfileDropdown = false;
+
+  addToggle() {
+    this.status = !this.status;
+  }
+
+  toggleNotificationDropdown() {
+    this.showNotificationDropdown = !this.showNotificationDropdown;
+    // Si vous souhaitez masquer l'autre dropdown lorsque celui-ci est ouvert
+    this.showProfileDropdown = false;
+  }
+
+  toggleProfileDropdown() {
+    this.showProfileDropdown = !this.showProfileDropdown;
+    // Si vous souhaitez masquer l'autre dropdown lorsque celui-ci est ouvert
+    this.showNotificationDropdown = false;
+  }
+
 }

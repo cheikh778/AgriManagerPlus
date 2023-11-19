7765225c879d890d8d9890535d80d34b21b4b5cd
Abdoulaye LY <LY028609@macly028609.orange-sonatel.com>
2023-11-19 00:15:54 +0000

    debug 403 error

--- src/app/shared/cors.interceptor.ts
+++ src/app/shared/cors.interceptor.ts
@@ -6,15 +6,19 @@ import {
   HttpEvent,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { LoginPageService } from "../login-page.service";
 
 @Injectable()
 export class CorsInterceptor implements HttpInterceptor {
+    constructor(private authService: LoginPageService) { }
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    const authToken = this.authService.getToken();
     const modifiedRequest = request.clone({
       setHeaders: {
+        Authorization: "Bearer " + authToken,
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': 'http://localhost:4200', 
       },

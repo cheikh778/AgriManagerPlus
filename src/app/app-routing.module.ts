736acde63697cb9b1fbccde52132c4fa7aacb17e
cheikh778 <locheikhndao778@gmail.com>
2023-11-23 13:07:03 +0000

    suppression de certains composants qui amenent des conflits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,12 @@ import { Page403Component } from './page403/page403.component';
 import { Page404Component } from './page404/page404.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { TermesComponent } from './termes/termes.component';
-import {AjoutCultureComponent} from "./Paysan/ajout-culture/ajout-culture.component";
 import { DashboardPComponent } from './Paysan/dashboard-p/dashboard-p.component';
 import { DashboardAComponent } from './Admin/dashboard-a/dashboard-a.component';
 import { AjoutUtilisateurComponent } from './Admin/ajout-utilisateur/ajout-utilisateur.component';
 import { ListeUtilisateursComponent } from './Admin/liste-utilisateurs/liste-utilisateurs.component';
 import { ListeProjetEnAttenteComponent } from './liste-projet-agricole/liste-projet-en-attente.component'
+import { AjoutCultureComponent } from './Paysan/ajout-culture/ajout-culture.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/accueil', pathMatch: 'full' },
@@ -27,11 +27,11 @@ const routes: Routes = [
   { path: 'paysanDashboard', component:DashboardPComponent},
   { path: 'adminDashboard', component:DashboardAComponent},
   { path:'pageNotAuthorized', component:Page403Component},
-  { path:'', component:Page404Component},
+  { path:'**', component:Page404Component},
   { path:'listeProjet', component:ListeProjetEnAttenteComponent},
   { path:'listeUser', component:ListeUtilisateursComponent},
   { path:'ajoutUser', component:AjoutUtilisateurComponent},
-  
+
 
 ];
 

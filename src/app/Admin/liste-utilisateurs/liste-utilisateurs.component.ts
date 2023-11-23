2fc2caf6680080f93c85b3cdad013ed4c317682c
geniedevisep <ai.niass@isepdiamniadio.edu.sn>
2023-11-23 22:40:40 +0000

    nez commit

--- src/app/Admin/liste-utilisateurs/liste-utilisateurs.component.ts
+++ src/app/Admin/liste-utilisateurs/liste-utilisateurs.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ListeProjetAgricolService } from 'src/app/liste-projet-agricol.service';
 import { ListeUtilisateurService } from 'src/app/liste-utilisateur.service';
 import { User } from 'src/app/modeles';
+import { NgZone } from '@angular/core';import { ChangeDetectorRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-liste-utilisateurs',
@@ -20,7 +22,7 @@ export class ListeUtilisateursComponent {
   errorMessage = "";
   sucessMessage= "";
 
-  constructor(private userSevice : ListeUtilisateurService){}
+  constructor(private userSevice : ListeUtilisateurService,private zone: NgZone,private cdr: ChangeDetectorRef){}
   
   ngOnInit(): void {
     this.userSevice.getListeUser().subscribe(
@@ -37,42 +39,44 @@ export class ListeUtilisateursComponent {
   )
 }
 
-
-
+// private getUsers() {
+//   this.userSevice.getListeUser().subscribe({
+//     next: (apps) => {
+//       this.user = apps;
+//     },
+//     error: (err) => {
+//       this.errorMessage = "Erreur de la requête";
+//     },
+//     complete: () => {
+//       this.sucessMessage = "Requête valide";
+//     }
+//   });
+// }
 blockUser(userId: number): void {
-  this.userSevice.bloquer(userId).subscribe(
-    {
-      next: () => {
-        this.sucessMessage = "Utilisateur bloqué avec succès";
-        // const userIndex = this.user.findIndex(user => user.id === userId);
-        // if (userIndex !== -1) {
-        //   this.user[userIndex].status = 'bloquer';
-        // }
-      },
-      error: (err) => {
-        this.errorMessage = "Erreur lors du blocage de l'utilisateur";
-      }
+  this.userSevice.bloquer(userId).subscribe({
+    next: () => {
+      this.sucessMessage = "Utilisateur bloqué avec succès";
+      this.updateUserStatus(userId, 'bloquer');
+      this.cdr.detectChanges();
+    },
+    error: (err) => {
+      this.errorMessage = "Erreur lors du blocage de l'utilisateur";
     }
-  );
+  });
 }
 
-// Function to unblock a user
 unblockUser(userId: number): void {
-  this.userSevice.debloquer(userId).subscribe(
-    {
-      next: () => {
-        this.sucessMessage = "Utilisateur débloqué avec succès";
-        // const userIndex = this.user.findIndex(user => user.id === userId);
-        // if (userIndex !== -1) {
-        //   this.user[userIndex].status = 'debloquer';
-        // }
-      },
-      error: (err) => {
-        this.errorMessage = "Erreur lors du déblocage de l'utilisateur";
-      }
+  this.userSevice.debloquer(userId).subscribe({
+    next: () => {
+      this.sucessMessage = "Utilisateur débloqué avec succès";
+      this.updateUserStatus(userId, 'debloquer');
+    },
+    error: (err) => {
+      this.errorMessage = "Erreur lors du déblocage de l'utilisateur";
     }
-  );
+  });
 }
+
 
 private updateUserStatus(userId: number, newStatus: string): number {
   const userIndex = this.user.findIndex(user => user.id === userId);

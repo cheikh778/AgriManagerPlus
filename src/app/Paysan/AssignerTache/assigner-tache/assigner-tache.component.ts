acb442e1651b0456cd8ef379816013df9f764a94
Zeynab-Thiandoum <s.thiandoum3@isepdiamniadion.edu.sn>
2023-11-28 10:01:47 +0000

    AssignerTache

---
+++ src/app/Paysan/AssignerTache/assigner-tache/assigner-tache.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assigner-tache',
+  templateUrl: './assigner-tache.component.html',
+  styleUrls: ['./assigner-tache.component.scss']
+})
+export class AssignerTacheComponent {
+
+}

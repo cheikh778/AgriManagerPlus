5750e66f629a6169f1a22c719499dbe0d42adb6e
cheikh778 <locheikhndao778@gmail.com>
2024-01-01 14:22:22 +0000

    nouveau changement

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+
+  constructor(private router: Router) {}
+
+
+  
+
+  scrollToSection(sectionId: string): void {
+    this.router.navigate(['/accueil'], { fragment: sectionId });
+  }
 
   formattedDateTime: string = '';
 
